90d86a0a89a37a1d94430b958be378935ce8e60f
chocoberu <43817454+chocoberu@users.noreply.github.com>
2019-01-03 15:51:12 +0900

    1장
    https://www.youtube.com/watch?v=jdv8erC7ML8&index=2&list=PLctzObGsrjfyevwpeEVQ9pxGVwZtS7gZK
    07:56

---
+++ 3d top-down shooting/Assets/Script/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(PlayerController))] // Player 스크립트를 오브젝트에 붙일 때 PlayerController 또한 같이 붙이도록 강요함
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5;
+    PlayerController controller;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<PlayerController>(); // PlayerController와 Player 스크립트가 같은 오브젝트에 붙어 있다고 가정
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+	}
+}
---
+++ 3d top-down shooting/Assets/Script/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody myRigidbody; // 충돌에 영향을 받도록
+    Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+    public void FixedUpdate() // 정기적이고 짧게 반복적으로 실행되어야함
+                              // 프레임이 저하되어도 프레임에 시간의 가중치를 곱해 실행되어 이동 속도를 유지
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime); // fixedDeltaTime은 두 fixedUpdate 메소드가 호출된 시간 간격
+    }
+}
