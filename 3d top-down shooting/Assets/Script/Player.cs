64bed7f4263a1cdd946253d03d184ae0e7c9ebec
chocoberu <43817454+chocoberu@users.noreply.github.com>
2019-01-03 22:58:29 +0900

    1장 완료

--- 3d top-down shooting/Assets/Script/Player.cs
+++ 3d top-down shooting/Assets/Script/Player.cs
@@ -7,16 +7,28 @@ public class Player : MonoBehaviour {
 
     public float moveSpeed = 5;
     PlayerController controller;
+    Camera viewCamera;
 	// Use this for initialization
 	void Start () {
         controller = GetComponent<PlayerController>(); // PlayerController와 Player 스크립트가 같은 오브젝트에 붙어 있다고 가정
+        viewCamera = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity);
 
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            //Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+        }
 	}
 }
--- 3d top-down shooting/Assets/Script/PlayerController.cs
+++ 3d top-down shooting/Assets/Script/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     Rigidbody myRigidbody; // 충돌에 영향을 받도록
     Vector3 velocity;
@@ -22,5 +23,10 @@ public class PlayerController : MonoBehaviour {
                               // 프레임이 저하되어도 프레임에 시간의 가중치를 곱해 실행되어 이동 속도를 유지
     {
         myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime); // fixedDeltaTime은 두 fixedUpdate 메소드가 호출된 시간 간격
+    }
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
     }
 }
