726264a22fdf21f5559b7f1ec7330a7246addcf3
chocoberu <43817454+chocoberu@users.noreply.github.com>
2019-01-04 16:42:07 +0900

    2~3장

---
+++ 3d top-down shooting/Assets/Script/GunController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public Transform weaponHold;
+    public Gun startingGun;
+    Gun equippedGun;
+
+    void Start()
+    {
+        if (startingGun != null)
+            EquipGun(startingGun);
+    }
+    public void EquipGun(Gun gunToEquip)
+    {
+        if(equippedGun != null)
+        {
+            Destroy(equippedGun.gameObject);
+        }
+        equippedGun = Instantiate(gunToEquip,weaponHold.transform.position,weaponHold.transform.rotation) as Gun;
+        equippedGun.transform.parent = weaponHold; // 총 오브젝트가 플레이어를 따라 움직이도록 weaponHold의 자식으로 넣어야함
+    }
+    public void Shoot()
+    {
+        if (equippedGun != null)
+            equippedGun.Shoot();
+    }
+}
--- 3d top-down shooting/Assets/Script/Player.cs
+++ 3d top-down shooting/Assets/Script/Player.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof(PlayerController))] // Player 스크립트를 오브젝트에 붙일 때 PlayerController 또한 같이 붙이도록 강요함
+[RequireComponent (typeof(GunController))] // Player 스크립트를 오브젝트에 붙일 때 GunController 또한 같이 붙이도록 강요함
 public class Player : MonoBehaviour {
 
     public float moveSpeed = 5;
     PlayerController controller;
     Camera viewCamera;
+    GunController gunController;
 	// Use this for initialization
 	void Start () {
         controller = GetComponent<PlayerController>(); // PlayerController와 Player 스크립트가 같은 오브젝트에 붙어 있다고 가정
+        gunController = GetComponent<GunController>();
         viewCamera = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        // 이동을 입력 받는 곳
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity);
 
+        // 바라보는 방향을 입력받는 곳
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         float rayDistance;
@@ -30,5 +35,8 @@ public class Player : MonoBehaviour {
             //Debug.DrawLine(ray.origin, point, Color.red);
             controller.LookAt(point);
         }
+        // 무기 조작 입력을 받는 곳
+        if (Input.GetMouseButton(0))
+            gunController.Shoot();
 	}
 }
