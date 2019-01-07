54f8140e440906738b00ba2003c812c83933e17f
chocoberu <43817454+chocoberu@users.noreply.github.com>
2019-01-07 14:49:44 +0900

    7장

--- 3d top-down shooting/Assets/Script/Enemy.cs
+++ 3d top-down shooting/Assets/Script/Enemy.cs
@@ -14,6 +14,7 @@ public class Enemy : LivingEntity {
     Transform target; // 공격 대상, Player
     Material skinMaterial;
     Color originalColor;
+    LivingEntity targetEntity;
 
     float attackDistanceThreshold = .5f; // 공격 거리 임계값, 공격할 수 있는 한계거리 (1.5m)
     float timeBetweenAttacks = 1;
@@ -21,6 +22,8 @@ public class Enemy : LivingEntity {
     float nextAttackTime;
     float myCollisionRadius; // 적의 충돌반경 반지름
     float targetCollisionRadius; // 플레이어의 충돌반경 반지름
+    float damage = 1;
+    bool hasTarget;
 
     // Use this for initialization
     protected override void Start () {
@@ -29,30 +32,42 @@ public class Enemy : LivingEntity {
         skinMaterial = GetComponent<Renderer>().material;
         originalColor = skinMaterial.color;
 
-        currentState = State.Chasing;
-        target = GameObject.FindGameObjectWithTag("Player").transform; // target을 Player로 지정
-        
-        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
-        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            currentState = State.Chasing;
+            hasTarget = true;
+            target = GameObject.FindGameObjectWithTag("Player").transform; // target을 Player로 지정
+            targetEntity = target.GetComponent<LivingEntity>();
+            targetEntity.OnDeath += OnTargetDeath;
 
-        StartCoroutine(UpdatePath()); // 코루틴 시작 (플레이어 추적)
+            myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+            targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
+
+            StartCoroutine(UpdatePath()); // 코루틴 시작 (플레이어 추적)
+        }
 	}
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Time.time > nextAttackTime)
+        if (hasTarget)
         {
-            float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
-            if (sqrDstToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2))
+            if (Time.time > nextAttackTime)
             {
-               nextAttackTime = Time.time + timeBetweenAttacks;
-               StartCoroutine(Attack());
+                float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
+                if (sqrDstToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2))
+                {
+                    nextAttackTime = Time.time + timeBetweenAttacks;
+                    StartCoroutine(Attack());
+                }
+
             }
-
         }
-
+    }
+    void OnTargetDeath()
+    {
+        hasTarget = false;
+        currentState = State.Idle;
     }
     IEnumerator Attack()
     {
@@ -68,10 +83,15 @@ public class Enemy : LivingEntity {
         float percent = 0;
 
         skinMaterial.color = Color.red;
+        bool hasAppliedDamage = false;
 
         while (percent <= 1)
         {
-
+            if (percent >= 0.5f && !hasAppliedDamage)
+            {
+                hasAppliedDamage = true;
+                targetEntity.TakeDamage(damage);
+            }
             percent += Time.deltaTime * attackSpeed;
             float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
             transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
@@ -87,7 +107,7 @@ public class Enemy : LivingEntity {
     {
         float refreshRate = .25f;
 
-        while (target != null)
+        while (hasTarget)
         {
             if (currentState == State.Chasing)
             {
--- 3d top-down shooting/Assets/Script/Projectile.cs
+++ 3d top-down shooting/Assets/Script/Projectile.cs
@@ -7,6 +7,19 @@ public class Projectile : MonoBehaviour {
     public LayerMask collisionMask;
     float speed = 10;
     float damage = 1;
+    float skinWidth = 0.1f;
+
+    float lifeTime = 3;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+        Collider[] initialCollisions = Physics.OverlapSphere(transform.position, 0.1f,collisionMask);
+        if(initialCollisions.Length > 0) // 총알이 생성됐을 때 어떤 충돌체 오브젝트와 이미 겹친 경우
+        {
+            OnHitObject(initialCollisions[0]);
+        }
+    }
 
     public void SetSpeed(float newSpeed)
     {
@@ -23,7 +36,7 @@ public class Projectile : MonoBehaviour {
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask,QueryTriggerInteraction.Collide))
+        if (Physics.Raycast(ray, out hit, moveDistance + skinWidth, collisionMask,QueryTriggerInteraction.Collide))
         {
             OnHitObject(hit);
         }
@@ -35,6 +48,15 @@ public class Projectile : MonoBehaviour {
         if(damageableObject != null)
         {
             damageableObject.TakeHit(damage, hit);
+        }
+        GameObject.Destroy(gameObject);
+    }
+    void OnHitObject(Collider c)
+    {
+        IDamageable damageableObject = c.GetComponent<IDamageable>();
+        if (damageableObject != null)
+        {
+            damageableObject.TakeDamage(damage);
         }
         GameObject.Destroy(gameObject);
     }
