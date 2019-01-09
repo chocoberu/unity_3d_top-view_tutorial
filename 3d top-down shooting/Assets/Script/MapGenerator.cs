8dc97b82207585fc8df09508dda8e8d6a0cdef27
chocoberu <43817454+chocoberu@users.noreply.github.com>
2019-01-09 14:43:39 +0900

    8장 중반
    https://www.youtube.com/watch?v=gIUVRYViG_g&t=2s
    06:57

---
+++ 3d top-down shooting/Assets/Script/MapGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+    public float outlienPercent;
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+    public void GenerateMap()
+    {
+        for(int x = 0; x < mapSize.x; x++)
+        {
+            for(int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y); //타일을 소환할 위치 계산을 위해
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
+            }
+        }
+    }
+}
