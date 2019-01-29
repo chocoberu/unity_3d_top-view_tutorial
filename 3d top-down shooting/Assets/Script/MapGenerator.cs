8e292e9b5ac5476a26043fbfcb3272b870252bc8
chocoberu <43817454+chocoberu@users.noreply.github.com>
2019-01-29 15:46:36 +0900

    10장 중반
    https://www.youtube.com/watch?v=2ycN6ZkWgOo
    11:01

--- 3d top-down shooting/Assets/Script/MapGenerator.cs
+++ 3d top-down shooting/Assets/Script/MapGenerator.cs
@@ -11,9 +11,14 @@ public class MapGenerator : MonoBehaviour {
     [Range(0,1)]
     public float outlienPercent;
 
+    [Range(0,1)]
+    public float obstaclePercent; // 장애물을 몇개 만들지 정하는 변수
+
     List<Coord> allTileCoords; //타일 좌표에 대한 리스트
     Queue<Coord> shuffledTileCoords;
     public int seed = 10; // 랜덤 생성 시 필요한 seed 값
+
+    Coord mapCentre;
 
     private void Start()
     {
@@ -30,6 +35,7 @@ public class MapGenerator : MonoBehaviour {
             }
         }
         shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(),seed));
+        mapCentre = new Coord((int)mapSize.x / 2, (int)mapSize.y / 2);
 
         string holderName = "Generated Map"; // 타일들을 자식으로 묶을 오브젝트의 이름
         if(transform.Find(holderName))
@@ -52,17 +58,57 @@ public class MapGenerator : MonoBehaviour {
             }
         }
 
+        bool[,] obstacleMap = new bool[(int)mapSize.x, (int)mapSize.y];
         //장애물 생성 과정
-        int obstacleCount = 10;
+        int obstacleCount = (int)(mapSize.x * mapSize.y * obstaclePercent); // 장애물 생성 개수
+        int currentObstacleCount = 0;
         for (int i = 0; i < obstacleCount; i++)
         {
             Coord randomCoord = GetRandomCoord();
-            Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
-            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity) as Transform;
-            newObstacle.parent = mapHolder;
+            obstacleMap[randomCoord.x, randomCoord.y] = true;
+            currentObstacleCount++;
+            if (randomCoord != mapCentre && MapIsFullyAccessible(obstacleMap, currentObstacleCount))
+            {
+                Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+                Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity) as Transform;
+                newObstacle.parent = mapHolder;
+            }
+            else
+            {
+                obstacleMap[randomCoord.x, randomCoord.y] = false;
+                currentObstacleCount --;
+            }
         }
     }
 
+    bool MapIsFullyAccessible(bool[,] obstacleMap, int currentObstacleCount)
+    {
+        bool[,] mapFlags = new bool[obstacleMap.GetLength(0), obstacleMap.GetLength(1)];
+        Queue<Coord> queue = new Queue<Coord>();
+        queue.Enqueue(mapCentre);
+        mapFlags[mapCentre.x, mapCentre.y] = true;
+
+        while(queue.Count > 0)
+        {
+            Coord tile = queue.Dequeue();
+
+            for(int x = -1; x <= 1; x++)
+            {
+                for(int y = -1; y <= 1; y++)
+                {
+                    int neighbourX = tile.x + x;
+                    int neighbourY = tile.y + y;
+                    if(x == 0 || y == 0)
+                    {
+                        if(neighbourX >= 0 && neighbourX < obstacleMap.GetLength(0) && neighbourY >= 0 && neighbourY < obstacleMap.GetLength(1))
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
     Vector3 CoordToPosition(int x,int y) //타일을 소환할 위치 계산을 위해
     {
         return new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
